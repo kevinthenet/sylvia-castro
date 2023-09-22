f17c07e2fbd2663c5183f743b6f8665b89a51930
Kevin Castro <me@kevincastro.dev>
2023-09-21 21:14:11 -0700

    setup i18n

---
+++ src/i18n/es.ts
@@ -0,0 +1,193 @@
+import { type Localization } from './index';
+
+export const es: Localization = {
+  homepage: {
+    heroSection: {
+      header: 'Serving San Diego and beyond',
+      subtitle: 'Find out why I have been a trusted name in the San Diego Area',
+      ctaButtons: {
+        about: 'Learn more about me',
+        contact: 'Get a quote',
+      },
+    },
+    serviceSection: {
+      header: 'Services',
+      serviceCategories: {
+        translation: {
+          name: 'Translation Services',
+          services: [
+            'Document Translation',
+            'Website Localization',
+            'Technical Translation',
+            'Medical Translation',
+            'Legal Translation',
+            'Business Translation',
+            'E-commerce Translation',
+            'Multilingual Content Creation',
+            'Certified Translations',
+            'Technical Manuals',
+          ],
+        },
+        interpretation: {
+          name: 'Interpretation Services',
+          services: [
+            'On-Site Interpretation',
+            'Video Conferencing Interpretation',
+            'Phone Interpretation',
+            'Simultaneous Interpretation',
+            'Consecutive Interpretation',
+            'Escort Interpretation',
+            'Medical Interpretation',
+            'Legal Interpretation',
+            'Community Interpretation',
+          ],
+        },
+        additional: {
+          name: 'Additional Services',
+          services: [
+            'Language Consulting',
+            'Localization Consultation',
+            'Cross-Cultural Communication Training',
+            'Desktop Publishing and Formatting',
+            'Quality Assurance and Language Testing',
+            'Language Pair Specialization',
+            'Custom Language Solutions',
+          ],
+        },
+      },
+    },
+    aboutSection: {
+      header: 'About me',
+      aboutText:
+        'As a translator and interpreter, I have done work for various organizations, translated and interpreted from English to Spanish on legal, social, family, employment, health, education, and government issues, among others. I had the pleasure of working for different public and private agencies. Translation work undergoes a multiple proofreading and cross-referencing process to assure that the meaning, context, tone, and message of all translated text use the most accurate and appropriate language and choice of words possible. On the other hand, interpretation will always require quick processing of information, and message rendition. Interpretation, either simultaneous or consecutive, requires knowledge of language registers and language equivalents. The accuracy of my translations and faithfulness of my interpretations will satisfy all of your demands. If you have a project that needs to be translated, or a meeting to be interpreted, feel free to contact me for a quote and to arrange an assignment.',
+      aboutButton: 'Read more',
+    },
+    recommendationSection: {
+      header: 'Trusted by public and private organizations for over 20 years',
+      recommendations: [
+        {
+          name: 'Rosemarie Perez',
+          image: '/rosemarie-perez.jpeg',
+          message:
+            'Sylvia Castro worked for me as a translator and interpreter for three years. She was excellent at her job. She has since received certification as an interpreter. This highly skilled individual would be an asset to organizations wanting to communicate in Spanish.Sylvia Castro worked for me as a translator and interpreter for three years. She was excellent at her job. She has since received certification as an interpreter. This highly skilled individual would be an asset to organizations wanting to communicate in Spanish.',
+        },
+        {
+          name: 'Rita Melton',
+          image: '/rita-melton.jpeg',
+          message:
+            'Sylvia has worked for Redwood City School District as a Translator for many years. I have seen her handle the stresses of translation with a calm focus. She takes the time to make sure she is accurate in her communication. She does a great job.',
+        },
+        {
+          name: 'Naomi Sigler Hunter, APR',
+          image: '/naomi-sigler-hunter.jpeg',
+          message:
+            'Sylvia is an extremely hard worker and very easy to work with. She translated writing for me under great tight deadlines and occasionally at inconvenient times, but never showed signs of stress. She is the type of worker who not only does what is asked of her, but goes beyond what she is personally responsible for to make sure the organization is doing top quality work. Sylvia is also an idea person, and makes very good suggestions that helped me do my own job better. I always enjoyed working with Sylvia and recommend her with enthusiasm!',
+        },
+        {
+          name: 'Hilary Paulson',
+          image: '/hilary-paulson.jpeg',
+          message:
+            'Sylvia worked as the translator for the Redwood City School Board meetings - working well with parents and community members often having to translate in pressurized environments with time constraints.',
+        },
+        {
+          name: 'Julia Morales',
+          message:
+            'Sylvia was one of the best students in the classroom who was of great help to the professor. She was fluent in both languages and a leader in the classroom.Sylvia was one of the best students in the classroom who was of great help to the professor. She was fluent in both languages and a leader in the classroom.',
+        },
+        {
+          name: 'Juanita Ramirez',
+          message:
+            'Sylvia is a professional in every way. I have observed her in translation situations with English and Spanish. She is very fluent in both English and Spanish. She has a wide vocabulary in both languages to provide the best possible translation to her clients. I would recommend her to any organization or business that needs quality translation.',
+        },
+      ],
+    },
+    ctaSection: {
+      header: 'Reach bigger audiences',
+      subtitle: 'Get a free quote',
+      button: 'Contact me',
+    },
+  },
+  about: {
+    header: 'About Me',
+    subtitle: 'Bridging language and cultural barriers',
+    missionSection: {
+      pictureAlt: 'Sylvia Castro Profile Picture',
+      header: 'My mission',
+      text: "¡Hola! I'm Sylvia Castro, a San Diego-based professional interpreter and translator, certified by both the American Translators Association (ATA) and the Northern California Translators Association (NCTA). My extensive experience encompasses legal, social, family, employment, health, education, and government contexts, among others, serving various public and private organizations. In my translation work, I emphasize precision through rigorous proofreading and cross-referencing, ensuring that translated content preserves not only the words but also the true essence, context, tone, and message. My interpretation expertise, whether simultaneous or consecutive, relies on a deep understanding of language registers and equivalents, guaranteeing seamless communication. Count on me for accurate translations and faithful interpretations that exceed your expectations. Feel free to contact me for project quotes and assignment arrangements. Let's bridge linguistic divides and facilitate effortless communication together. ¡Contáctame hoy mismo! (Contact me today!)",
+      stats: [{ name: 'Experience', value: 'Over 20 years' }],
+    },
+    valuesSection: [
+      {
+        ref: '01',
+        name: 'Accuracy',
+        description:
+          'I am committed to delivering accurate translations and interpretations that faithfully convey the intended meaning and match the register of your message.',
+      },
+      {
+        ref: '02',
+        name: 'Professionalism',
+        description:
+          'I am dedicated to maintaining high professional standards, including punctuality, confidentiality, and ethical conduct.',
+      },
+      {
+        ref: '03',
+        name: 'Excellence',
+        description:
+          'I dedicate myself to providing top-of-the line service to suit your needs. I will do my best to meet and exceed your standard of excellence.',
+      },
+      {
+        ref: '04',
+        name: 'Respect',
+        description:
+          'I aim to be transparent in my pricing, processes, and project timelines, ensuring you know what to expect.',
+      },
+    ],
+  },
+  contact: {
+    header: 'Unlock opportunities with Spanish-speakers',
+    subtitle:
+      "I'd love to know how I could be of help. I provide free quotes, so please feel free to reach out for any project, big or small!",
+    info: {
+      postalAddress: 'Postal Address',
+      phoneNumber: 'Phone Number',
+      email: 'Email',
+    },
+    form: {
+      fullName: 'Full Name',
+      email: 'Email',
+      message: 'Message',
+    },
+  },
+  404: {
+    header: 'Page not found',
+    subtitle: 'Please check the URL in the address bar and try again.',
+    action: 'Go back home',
+  },
+  header: {
+    logoAlt: 'Sylvia Castro Logo',
+    description:
+      'Sylvia Castro is a Spanish language translator and interpreter based in the San Diego Area with over 20 years of experience.',
+    toggleLanguage: 'Toggle Language',
+    toggleTheme: 'Toggle Theme',
+    toggleMenu: 'Toggle Menu',
+  },
+  footer: {
+    logoAlt: 'Sylvia Castro Logo',
+    description:
+      'Sylvia Castro is a Spanish language translator and interpreter based in the San Diego Area with over 20 years of experience.',
+    copyright: 'Copyright © 2002-2023 Sylvia Castro',
+    social: {
+      title: 'Social',
+    },
+    internal: {
+      title: 'Internal',
+      home: 'Home',
+      about: 'About',
+      contact: 'Contact',
+    },
+    legal: {
+      title: 'Legal',
+      privacy: 'Privacy',
+    },
+  },
+};
---
+++ src/i18n/index.ts
@@ -0,0 +1,121 @@
+import { en } from './en';
+import { es } from './es';
+
+export type Localization = {
+  homepage: {
+    heroSection: {
+      header: string;
+      subtitle: string;
+      ctaButtons: {
+        about: string;
+        contact: string;
+      };
+    };
+    serviceSection: {
+      header: string;
+      serviceCategories: {
+        translation: {
+          name: string;
+          services: string[];
+        };
+        interpretation: {
+          name: string;
+          services: string[];
+        };
+        additional: {
+          name: string;
+          services: string[];
+        };
+      };
+    };
+    aboutSection: {
+      header: string;
+      aboutText: string;
+      aboutButton: string;
+    };
+    recommendationSection: {
+      header: string;
+      recommendations: {
+        name: string;
+        image?: string;
+        message: string;
+      }[];
+    };
+    ctaSection: {
+      header: string;
+      subtitle: string;
+      button: string;
+    };
+  };
+  about: {
+    header: string;
+    subtitle: string;
+    missionSection: {
+      pictureAlt: string;
+      header: string;
+      text: string;
+      stats: {
+        name: string;
+        value: string;
+      }[];
+    };
+    valuesSection: {
+      ref: string;
+      name: string;
+      description: string;
+    }[];
+  };
+  contact: {
+    header: string;
+    subtitle: string;
+    info: {
+      postalAddress: string;
+      phoneNumber: string;
+      email: string;
+    };
+    form: {
+      fullName: string;
+      email: string;
+      message: string;
+    };
+  };
+  404: {
+    header: string;
+    subtitle: string;
+    action: string;
+  };
+  header: {
+    logoAlt: string;
+    description: string;
+    toggleLanguage: string;
+    toggleTheme: string;
+    toggleMenu: string;
+  };
+  footer: {
+    logoAlt: string;
+    description: string;
+    copyright: string;
+    social: {
+      title: string;
+    };
+    internal: {
+      title: string;
+      home: string;
+      about: string;
+      contact: string;
+    };
+    legal: {
+      title: string;
+      privacy: string;
+    };
+  };
+};
+
+type Language = 'en' | 'es';
+
+type Messages = Record<Language, Localization>;
+
+export const messages: Messages = {
+  en,
+  es,
+};
