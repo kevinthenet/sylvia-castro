595244af322d5d7ca1f0f45af72b5ccf28c393d2
Kevin Castro <me@kevincastro.dev>
2023-10-30 13:30:23 -0700

    update site icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import webmanifest from "astro-webmanifest";
-import compress from "astro-compress";
+import webmanifest from 'astro-webmanifest';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sylvia-castro.com",
   integrations: [robotsTxt(), sitemap(), tailwind(), webmanifest({
     name: 'Sylvia Castro: Spanish Interpreter/Translator',
-    icon: 'public/sylvia-castro-logo.png',
+    icon: 'public/sylvia-castro-icon.png',
     description: 'Sylvia Castro is a Spanish Interpreter/Translator based out of San Diego, CA with over 20 years experience.',
     start_url: '/',
   }), compress(),],
